5de4ce503201e399c603ac797d44d48e9ae173ba
Christopher Aliprantis <aliprantis@proton.me>
2026-06-05 16:52:09 -0400

    stuff

--- src/MazeGame.c
+++ src/MazeGame.c
@@ -24,11 +24,69 @@ void PrintMaze(Maze maze)
     }
 }
 
+void GrowBranch(Maze* maze, int start_x, int start_y)
+{
+    int cx = start_x;
+    int cy = start_y;
+
+    for (int i = 0; i < 8; i++)
+    {
+        int directions[4][2] = { {0, -1}, {0, 1}, {-1, 0}, {1, 0} };
+        int valid_dirs[4];
+        int valid_count = 0;
+
+        for (int d = 0; d < 4; d++)
+        {
+            int nx = cx + directions[d][0];
+            int ny = cy + directions[d][1];
+
+            if (nx >= 1 && nx <= 13 && ny >= 1 && ny <= 5)
+            {
+                if (maze->grid[ny][nx] == '#')
+                {
+                    int open_neighbors = 0;
+                    for (int n = 0; n < 4; n++)
+                    {
+                        int nnx = nx + directions[n][0];
+                        int nny = ny + directions[n][1];
+                        if (nnx >= 0 && nnx <= 14 && nny >= 0 && nny <= 6)
+                        {
+                            if (maze->grid[nny][nnx] == ' ')
+                            {
+                                open_neighbors++;
+                            }
+                        }
+                    }
+
+                    if (open_neighbors <= 1)
+                    {
+                        valid_dirs[valid_count] = d;
+                        valid_count++;
+                    }
+                }
+            }
+        }
+
+        if (valid_count == 0) break;
+
+        int chosen_dir = valid_dirs[rand() % valid_count];
+        cx += directions[chosen_dir][0];
+        cy += directions[chosen_dir][1];
+
+        maze->grid[cy][cx] = ' ';
+
+        if (rand() % 10 < 3)
+        {
+            GrowBranch(maze, cx, cy);
+        }
+    }
+}
+
 Maze CreateMaze()
 {
     Maze maze;
 
-    static char filledmaze[15][16] =
+    static char filledmaze[7][16] =
     {
         "###############",
         "###############",
@@ -57,19 +115,20 @@ Maze CreateMaze()
     maze.player[1] = maze.start[1];
 
     coord currentmazemakingcoord;
-    currentmazemakingcoord[0] = maze.player[0];
-    currentmazemakingcoord[1] = maze.player[1];
+    currentmazemakingcoord[0] = maze.start[0];
+    currentmazemakingcoord[1] = 1;
 
     maze.grid[maze.start[1]][maze.start[0]] = ' ';
     maze.grid[maze.end[1]][maze.end[0]] = ' ';
 
-    while (currentmazemakingcoord[0] != maze.end[0] || currentmazemakingcoord[1] != maze.end[1])
+    while (currentmazemakingcoord[0] != maze.end[0] || currentmazemakingcoord[1] != 5)
     {
         maze.grid[currentmazemakingcoord[1]][currentmazemakingcoord[0]] = ' ';
+
         int next_x = currentmazemakingcoord[0];
         int next_y = currentmazemakingcoord[1];
 
-        if (currentmazemakingcoord[1] < maze.end[1] && (rand() % 2 == 0 || currentmazemakingcoord[0] == maze.end[0]))
+        if (currentmazemakingcoord[1] < 5 && (rand() % 2 == 0 || currentmazemakingcoord[0] == maze.end[0]))
         {
             next_y++;
         }
@@ -81,23 +140,26 @@ Maze CreateMaze()
         {
             next_x--;
         }
-        if ((next_x >= 1 && next_x <= 6 && next_y >= 1 && next_y <= 6) ||
-            (next_x == maze.start[0] && next_y == maze.start[1]) ||
-            (next_x == maze.end[0] && next_y == maze.end[1]))
+
+        if (next_x >= 1 && next_x <= 13 && next_y >= 1 && next_y <= 5)
         {
             currentmazemakingcoord[0] = next_x;
             currentmazemakingcoord[1] = next_y;
         }
-        else
+    }
+
+    maze.grid[5][maze.end[0]] = ' ';
+
+    for (int y = 1; y <= 5; y++)
+    {
+        for (int x = 1; x <= 13; x++)
         {
-            if (currentmazemakingcoord[0] != maze.end[0])
+            if (maze.grid[y][x] == ' ')
             {
-                if (currentmazemakingcoord[0] < maze.end[0]) currentmazemakingcoord[0]++;
-                else currentmazemakingcoord[0]--;
-            }
-            else
-            {
-                currentmazemakingcoord[1]++;
+                if (rand() % 100 < 40)
+                {
+                    GrowBranch(&maze, x, y);
+                }
             }
         }
     }
